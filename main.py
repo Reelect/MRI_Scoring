af85ece6bfbc4c222a44b0d7bffa2374f0b2502e
Reelect <rejaealsun@gist.ac.kr>
2023-06-01 08:43:42 +0900

    final update2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 import numpy as np
 import math
-from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QSpinBox, QDesktopWidget, QPushButton, QDoubleSpinBox)
-from PyQt5.QtGui import QIcon
+import requests
+from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QSpinBox, QDesktopWidget, QPushButton, QDoubleSpinBox, QFrame, QLineEdit, QComboBox, QCheckBox)
+from PyQt5.QtGui import QIcon, QPixmap, QFont
+from PyQt5.QtCore import Qt
 from PyQt5 import QtCore
 from qt_material import apply_stylesheet, QtStyleTools
 
@@ -58,36 +60,122 @@ class MyApp(QWidget, QtStyleTools):
         self.setLayout(grid)
         self.sc = MplCanvas()
         toolbar = NavigationToolbar(self.sc, self)
-        self.spinboxs = [QDoubleSpinBox(), QDoubleSpinBox(), QSpinBox()]
+        self.spinboxs = [QDoubleSpinBox(), QDoubleSpinBox(), QSpinBox(), QDoubleSpinBox()]
         btn = QPushButton("결과 확인", self)
         btn.clicked.connect(self.get_result)
         for spinbox in self.spinboxs:
             spinbox.setMinimum(1)
         self.spinboxs[0].setMaximum(1400000)
+        self.spinboxs[0].setValue(500000)
         self.spinboxs[1].setMaximum(10)
         self.spinboxs[2].setMaximum(10000)
+        self.spinboxs[2].setValue(1000)
+        self.spinboxs[3].setMaximum(10)
+        self.spinboxs[3].setValue(2.5)
 
         self.spinboxs[0].setSingleStep(1000)
 
-        grid.addWidget(QLabel('면적: '), 0, 0, alignment=QtCore.Qt.AlignRight)
-        grid.addWidget(QLabel('가로:세로 비율: '), 0, 3, alignment=QtCore.Qt.AlignRight)
-        grid.addWidget(QLabel('세대수: '), 0, 6, alignment=QtCore.Qt.AlignRight)
+        # 구역 특성 설정 spinbox
+        self.checkboxs = [
+            QCheckBox('세대 당 구성원 수', self),
+            QCheckBox('실거주 세대 비율', self),
+            QCheckBox('주요 나이대 설정', self),
+            QCheckBox('1일 평균 주문 비율', self)
+        ]
+        self.type = QComboBox(self)
+        txt = ["아파트", "산업 단지", "병원"]
+        self.type.addItems(txt)
+        self.property_boxs = [QSpinBox(), QSpinBox(), QSpinBox(), QSpinBox()]
+        self.property_boxs[0].setMaximum(20)
+        self.property_boxs[0].setValue(3)
+        self.property_boxs[0].setMinimum(1)
+        self.property_boxs[0].setSuffix("명")
 
-        grid.addWidget(self.spinboxs[0], 0, 1)
-        grid.addWidget(self.spinboxs[1], 0, 4)
-        grid.addWidget(self.spinboxs[2], 0, 7)
+        self.property_boxs[1].setMaximum(100)
+        self.property_boxs[1].setValue(75)
+        self.property_boxs[1].setMinimum(1)
+        self.property_boxs[1].setSuffix("%")
 
-        grid.addWidget(QLabel('미터제곱'), 0, 2)
-        grid.addWidget(QLabel(':1'), 0, 5)
-        grid.addWidget(QLabel('세대'), 0, 8)
-        grid.addWidget(btn, 1, 3, 1, 2)
-        grid.addWidget(toolbar, 2, 0, 1, 8)
-        grid.addWidget(self.sc, 3, 0, 1, 8)
+        self.property_boxs[2].setMaximum(100)
+        self.property_boxs[2].setValue(30)
+        self.property_boxs[2].setMinimum(1)
+        self.property_boxs[2].setSuffix("세")
+
+        self.property_boxs[3].setMaximum(100)
+        self.property_boxs[3].setValue(25)
+        self.property_boxs[3].setMinimum(0)
+        self.property_boxs[3].setSuffix("%")
+
+
+        # google map api
+
+        self.lineEdit = QLineEdit(self)
+        self.cmb = QComboBox(self)
+        txt = [str(i) for i in range(1, 21)]
+        self.cmb.addItems(txt)
+        self.cmb.setCurrentIndex(15)
+        self.mbtn = QPushButton('구역 검색', self)
+        self.img = QLabel('', self)
+        self.img.setFrameStyle(QFrame.Box)
+        self.mbtn.clicked.connect(self.map_search)
+
+        # map alignment
+        grid.addWidget(QLabel('구역 검색 '), 0, 0, alignment=QtCore.Qt.AlignRight)
+        grid.addWidget(self.lineEdit, 0, 1, 1, 6)
+        grid.addWidget(self.mbtn, 0, 7, 1, 2)
+        grid.addWidget(self.img, 1, 0, 7, 9)
+
+        #  구역 특성 설정 섹션
+        grid.addWidget(QLabel('구역 특성 설정'), 8, 0, alignment=QtCore.Qt.AlignLeft)
+
+        grid.addWidget(QLabel('구역 종류 : '), 9, 0, alignment=QtCore.Qt.AlignRight)
+        grid.addWidget(self.type, 9, 1, 1, 1)
+
+        grid.addWidget(self.checkboxs[0], 10, 0, 1, 2)
+        grid.addWidget(self.property_boxs[0], 10, 2)
+
+        grid.addWidget(self.checkboxs[1], 10, 3, 1, 2)
+        grid.addWidget(self.property_boxs[1], 10, 5)
+
+        grid.addWidget(self.checkboxs[2], 11, 0, 1, 2)
+        grid.addWidget(self.property_boxs[2], 11, 2)
+
+        grid.addWidget(self.checkboxs[3], 11, 3, 1, 2)
+        grid.addWidget(self.property_boxs[3], 11, 5)
+
+
+
+        """
+        grid.addWidget(QLabel('면적: '), 0, 12, alignment=QtCore.Qt.AlignRight)
+        grid.addWidget(self.spinboxs[0], 0, 13)
+        grid.addWidget(QLabel('미터제곱'), 0, 14)
+        
+
+        grid.addWidget(QLabel('가로:세로 비율: '), 0, 15, alignment=QtCore.Qt.AlignRight)
+        grid.addWidget(self.spinboxs[1], 0, 16)
+        grid.addWidget(QLabel(':1'), 0, 17)
+
+        grid.addWidget(QLabel('세대수: '), 0, 18, alignment=QtCore.Qt.AlignRight)
+        grid.addWidget(self.spinboxs[2], 0, 19)
+        grid.addWidget(QLabel('세대'), 0, 20)
+
+        grid.addWidget(QLabel('세대 당 구성원'), 0, 21, alignment=QtCore.Qt.AlignRight)
+        grid.addWidget(self.spinboxs[3], 0, 22)
+        grid.addWidget(QLabel('명'), 0, 23)
+        """
+
+        grid.addWidget(btn, 0, 10)
+        grid.addWidget(toolbar, 0, 9)
+        grid.addWidget(self.sc, 1, 9, 10, 2)
+
+
+
+
 
 
         self.setWindowTitle('아파트 단지 면적 및 세대수에 따른 필요 배송 로봇 수 계산기')
         self.setWindowIcon(QIcon('logo.png'))
-        self.resize(1500, 900)
+        self.resize(1800, 900)
         self.center()
         self.show()
 
@@ -101,11 +189,12 @@ class MyApp(QWidget, QtStyleTools):
         velocity = 9
         path = math.sqrt(self.spinboxs[0].value()/self.spinboxs[1].value()) * (1 + self.spinboxs[1].value()) * 1.5
         delivery = path / (velocity * 60)
-        order_lunch = self.spinboxs[2].value() / 9
-        order_dinner = self.spinboxs[2].value() / 7
-        order_normal = self.spinboxs[2].value() / 5.5
-        order_normal2 = self.spinboxs[2].value() / 4.7
-        max_time = (60 / delivery) * 15 + 120
+        family = self.spinboxs[3].value() / 2.5
+        order_lunch = family * self.spinboxs[2].value() / 9
+        order_dinner = family * self.spinboxs[2].value() / 7
+        order_normal = family * self.spinboxs[2].value() / 5.5
+        order_normal2 = family * self.spinboxs[2].value() / 4.7
+
         # take x, y
         xs = np.linspace(1, 300, 3000)
         ys = np.linspace(15, 15, 3000)
@@ -219,6 +308,32 @@ class MyApp(QWidget, QtStyleTools):
         self.sc.normal2.grid()
         self.sc.order_num.grid()
         self.sc.draw()
+
+    def map_search(self):
+        BASE_URL = 'https://maps.googleapis.com/maps/api/staticmap?'
+        API_KEY = 'AIzaSyAa3teUmXVM47Z5wzAQ5pmPnFGhKPH-z5o'
+        ZOOM = self.cmb.currentIndex() + 1
+        CITY = self.lineEdit.text()
+
+        W = self.img.width()
+        H = self.img.height()
+
+        URL = (BASE_URL
+               + f'center={CITY}'
+               + f'&zoom={ZOOM}'
+               + f'&size={W}x{H}&scale=2'
+               + '&markers=color:red%7Clabel:S%7C' + CITY
+               + f'&key={API_KEY}')
+
+        # HTTP request
+        response = requests.get(URL)
+
+        # image scaled and draw
+        img = QPixmap()
+        img.loadFromData(response.content)
+        img = img.scaled(img.width() // 2, img.height() // 2, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+
+        self.img.setPixmap(img)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
